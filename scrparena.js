02da1fa46999fdafa1a5fdb451812e42dac9bb98
luyan-tamec <contatestxxx6@gmail.com>
2026-03-22 01:41:32 -0300

    integ.ferramenta.votaçao

--- scrparena.js
+++ scrparena.js
@@ -1,6 +1,6 @@
 /* ===== CONFIG IMAGENS ===== */
 
-const BONECOS_PATH = "bonecos/"; // pasta raiz dos png
+const BONECOS_PATH = "bonecos/";
 const BONECOS_LIST = [
     "image (1).png",
     "image (2).png",
@@ -35,11 +35,11 @@ const BONECOS_LIST = [
     "image (31).png",
     "image (32).png"
 ];
-/* ================= CONFIG ================= */
 
-const channelName = "isaroza_";
 
-const USER_COOLDOWN = 15; // 15s por usuário
+const channelName = "luyan_tamec";
+
+const USER_COOLDOWN = 15000; // 15s por usuário
 const GLOBAL_COOLDOWN = 5000; // 5s entre spawns globais
 const MAX_BONECOS = 30;
 
@@ -47,8 +47,8 @@ const MAX_BONECOS = 30;
 
 const arena = document.getElementById("arena");
 
-const activeUsers = new Map();      // usuário -> boneco
-const userCooldowns = new Map();    // usuário -> timestamp
+const activeUsers = new Map();    
+const userCooldowns = new Map();  
 let lastGlobalSpawn = 0;
 
 /* ================= Z-INDEX DA ARENA OU DIV ================= */
@@ -80,12 +80,13 @@ client.on("message", (channel, tags, message, self) => {
 function handleJoin(username) {
     const now = Date.now();
 
-    // Anti-spam global
+    // Anti-spam geralzao
     if (now - lastGlobalSpawn < GLOBAL_COOLDOWN) return;
 
-    // Anti-spam individual
+    // Anti-spam
     if (userCooldowns.has(username)) {
         if (now - userCooldowns.get(username) < USER_COOLDOWN) return;
+        
     }
 
     // Limite de 1 boneco por usuário
@@ -138,7 +139,7 @@ function spawnBoneco(username) {
     activeUsers.set(username, data);
 }
 
-/* ============== MOVIMENTO VAI E VOLTA ============== */
+/* ============== VAI E VOLTA ============== */
 
 function update() {
 
