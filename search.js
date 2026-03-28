d38611212d256874f106abfdcd583991f3f85381
luyan-tamec <contatestxxx6@gmail.com>
2026-03-28 20:10:35 -0300

    add barra de pesquisa

---
+++ search.js
@@ -0,0 +1,24 @@
+const searchInput = document.getElementById("search")
+
+searchInput.addEventListener("input",(event)=>{
+    const value= formatString(event.target.value)  ;
+    const items = document.querySelectorAll(".tagNome")
+
+    items.forEach(item => {
+        if(formatString(item.textContent).indexOf(value) !== -1) {
+            item.style.display= "block"
+        } else {
+             item.style.display= "none"
+        }
+        
+    })
+    
+})
+
+function formatString(value) {
+    return value
+    .toLowerCase()
+    .trim()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '');
+}
